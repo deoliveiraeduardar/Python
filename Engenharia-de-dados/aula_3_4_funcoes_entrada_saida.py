37ab2524153301bde121e539f53af75938c194d1
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2024-09-25 18:17:55 -0300

    Update aula_3_4_funcoes_entrada_saida.py

--- Engenharia-de-dados/aula_3_4_funcoes_entrada_saida.py
+++ Engenharia-de-dados/aula_3_4_funcoes_entrada_saida.py
@@ -1,1 +1,5 @@
-nome= input ('Informe o seu nome: ')
+nome = input('Informe o seu nome: ')
+idade = input('Informe sua idade: ')
+print(nome, idade)
+print('teste', end=' ')
+print(nome, idade, end='... \n')
